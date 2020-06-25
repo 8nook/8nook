c417ae182d5ff7cae980f47d7c5d7639f1ba0ecc
Ukai <67305693+8nook@users.noreply.github.com>
2020-06-24 22:51:20 -0400

    Add files via upload

---
+++ ideologies.js
@@ -0,0 +1,444 @@
+ideologies = [
+    {
+        "name": "AOET",
+        "stats": {
+            "econ": 58,
+            "dipl": 58,
+            "govt": 58,
+            "scty": 58
+        }
+    },
+    {
+        "name": "AOEF",
+        "stats": {
+            "econ": 58,
+            "dipl": 58,
+            "govt": 58,
+            "scty": 42
+        }
+    },
+    {
+        "name": "AOCT",
+        "stats": {
+            "econ": 58,
+            "dipl": 58,
+            "govt": 42,
+            "scty": 58
+        }
+    },
+    {
+        "name": "AOCF",
+        "stats": {
+            "econ": 58,
+            "dipl": 58,
+            "govt": 42,
+            "scty": 42
+        }
+    },
+    {
+        "name": "AMET",
+        "stats": {
+            "econ": 58,
+            "dipl": 42,
+            "govt": 58,
+            "scty": 58
+        }
+    },
+    {
+        "name": "AMEF",
+        "stats": {
+            "econ": 58,
+            "dipl": 42,
+            "govt": 58,
+            "scty": 42
+        }
+    },
+    {
+        "name": "AMCT",
+        "stats": {
+            "econ": 58,
+            "dipl": 42,
+            "govt": 42,
+            "scty": 58
+        }
+    },
+    {
+        "name": "AMCF",
+        "stats": {
+            "econ": 58,
+            "dipl": 42,
+            "govt": 42,
+            "scty": 42
+        }
+    },
+    {
+        "name": "SOET",
+        "stats": {
+            "econ": 42,
+            "dipl": 58,
+            "govt": 58,
+            "scty": 58
+        }
+    },
+    {
+        "name": "SOEF",
+        "stats": {
+            "econ": 42,
+            "dipl": 58,
+            "govt": 58,
+            "scty": 42
+        }
+    },
+    {
+        "name": "SOCT",
+        "stats": {
+            "econ": 42,
+            "dipl": 58,
+            "govt": 42,
+            "scty": 58
+        }
+    },
+    {
+        "name": "SOCF",
+        "stats": {
+            "econ": 42,
+            "dipl": 58,
+            "govt": 42,
+            "scty": 42
+        }
+    },
+    {
+        "name": "SMET",
+        "stats": {
+            "econ": 42,
+            "dipl": 42,
+            "govt": 58,
+            "scty": 58
+        }
+    },
+    {
+        "name": "SMEF",
+        "stats": {
+            "econ": 42,
+            "dipl": 42,
+            "govt": 58,
+            "scty": 42
+        }
+    },
+    {
+        "name": "SMCT",
+        "stats": {
+            "econ": 42,
+            "dipl": 42,
+            "govt": 42,
+            "scty": 58
+        }
+    },
+    {
+        "name": "SMCF",
+        "stats": {
+            "econ": 42,
+            "dipl": 42,
+            "govt": 42,
+            "scty": 42
+        }
+    },
+    {
+        "name": "xxxx - Absurdly Balanced",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "xOET",
+        "stats": {
+            "econ": 50,
+            "dipl": 58,
+            "govt": 58,
+            "scty": 58
+        }
+    },
+    {
+        "name": "xOEF",
+        "stats": {
+            "econ": 50,
+            "dipl": 58,
+            "govt": 58,
+            "scty": 42
+        }
+    },
+    {
+        "name": "xOCT",
+        "stats": {
+            "econ": 50,
+            "dipl": 58,
+            "govt": 42,
+            "scty": 58
+        }
+    },
+    {
+        "name": "xOCF",
+        "stats": {
+            "econ": 50,
+            "dipl": 58,
+            "govt": 42,
+            "scty": 42
+        }
+    },
+    {
+        "name": "xMET",
+        "stats": {
+            "econ": 50,
+            "dipl": 42,
+            "govt": 58,
+            "scty": 58
+        }
+    },
+    {
+        "name": "xMEF",
+        "stats": {
+            "econ": 50,
+            "dipl": 42,
+            "govt": 58,
+            "scty": 42
+        }
+    },
+    {
+        "name": "xMCT",
+        "stats": {
+            "econ": 50,
+            "dipl": 42,
+            "govt": 42,
+            "scty": 58
+        }
+    },
+    {
+        "name": "xMCF",
+        "stats": {
+            "econ": 50,
+            "dipl": 42,
+            "govt": 42,
+            "scty": 42
+        }
+    },
+    {
+        "name": "AxET",
+        "stats": {
+            "econ": 58,
+            "dipl": 50,
+            "govt": 58,
+            "scty": 58
+        }
+    },
+    {
+        "name": "AxEF",
+        "stats": {
+            "econ": 58,
+            "dipl": 50,
+            "govt": 58,
+            "scty": 42
+        }
+    },
+    {
+        "name": "AxCT",
+        "stats": {
+            "econ": 58,
+            "dipl": 50,
+            "govt": 42,
+            "scty": 58
+        }
+    },
+    {
+        "name": "AxCF",
+        "stats": {
+            "econ": 58,
+            "dipl": 50,
+            "govt": 42,
+            "scty": 42
+        }
+    },
+    {
+        "name": "SxET",
+        "stats": {
+            "econ": 42,
+            "dipl": 50,
+            "govt": 58,
+            "scty": 58
+        }
+    },
+    {
+        "name": "SxEF",
+        "stats": {
+            "econ": 42,
+            "dipl": 50,
+            "govt": 58,
+            "scty": 42
+        }
+    },
+    {
+        "name": "SxCT",
+        "stats": {
+            "econ": 42,
+            "dipl": 50,
+            "govt": 42,
+            "scty": 58
+        }
+    },
+    {
+        "name": "SxCF",
+        "stats": {
+            "econ": 42,
+            "dipl": 50,
+            "govt": 42,
+            "scty": 42
+        }
+    },
+
+    {
+        "name": "AOxT",
+        "stats": {
+            "econ": 58,
+            "dipl": 58,
+            "govt": 50,
+            "scty": 58
+        }
+    },
+    {
+        "name": "AOxF",
+        "stats": {
+            "econ": 58,
+            "dipl": 58,
+            "govt": 50,
+            "scty": 42
+        }
+    },
+    {
+        "name": "AMxT",
+        "stats": {
+            "econ": 58,
+            "dipl": 42,
+            "govt": 50,
+            "scty": 58
+        }
+    },
+    {
+        "name": "AMxF",
+        "stats": {
+            "econ": 58,
+            "dipl": 42,
+            "govt": 50,
+            "scty": 42
+        }
+    },
+    {
+        "name": "SOxT",
+        "stats": {
+            "econ": 42,
+            "dipl": 58,
+            "govt": 50,
+            "scty": 58
+        }
+    },
+    {
+        "name": "SOxF",
+        "stats": {
+            "econ": 42,
+            "dipl": 58,
+            "govt": 50,
+            "scty": 42
+        }
+    },
+    {
+        "name": "SMxT",
+        "stats": {
+            "econ": 42,
+            "dipl": 42,
+            "govt": 50,
+            "scty": 58
+        }
+    },
+    {
+        "name": "SMxF",
+        "stats": {
+            "econ": 42,
+            "dipl": 42,
+            "govt": 50,
+            "scty": 42
+        }
+    },
+    {
+        "name": "AOEx",
+        "stats": {
+            "econ": 58,
+            "dipl": 58,
+            "govt": 58,
+            "scty": 50
+        }
+    },
+    {
+        "name": "AOCx",
+        "stats": {
+            "econ": 58,
+            "dipl": 58,
+            "govt": 42,
+            "scty": 50
+        }
+    },
+    {
+        "name": "AMEx",
+        "stats": {
+            "econ": 58,
+            "dipl": 42,
+            "govt": 58,
+            "scty": 50
+        }
+    },
+    {
+        "name": "AMCx",
+        "stats": {
+            "econ": 58,
+            "dipl": 42,
+            "govt": 42,
+            "scty": 50
+        }
+    },
+    {
+        "name": "SOEx",
+        "stats": {
+            "econ": 42,
+            "dipl": 58,
+            "govt": 58,
+            "scty": 50
+        }
+    },
+    {
+        "name": "SOCx",
+        "stats": {
+            "econ": 42,
+            "dipl": 58,
+            "govt": 42,
+            "scty": 50
+        }
+    },
+    {
+        "name": "SMEx",
+        "stats": {
+            "econ": 42,
+            "dipl": 42,
+            "govt": 58,
+            "scty": 50
+        }
+    },
+    {
+        "name": "SMCx",
+        "stats": {
+            "econ": 42,
+            "dipl": 42,
+            "govt": 42,
+            "scty": 50
+        }
+    }
+];
---
+++ questions.js
@@ -0,0 +1,632 @@
+questions = [
+    {
+        "question": "At its core, Animal Crossing is a game about getting away from reality.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It just makes sense to plan out your tasks before you start up the game.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "I wish I could trade with any player through the mail rather than having to visit them.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I spend more time playing Animal Crossing than I care to admit.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I enjoy sharing my creations with other players.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I often sit around and do nothing, just to take in the scenery.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "My ultimate goal is to build a breathtaking island.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 5
+        }
+    },
+    {
+        "question": "I use planning tools or sketches before I do major terraforming.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Sometimes I time travel to speed up progress or get some extra resources.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "There's no need to chitchat during trades with other players.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I genuinely enjoy speaking to my villagers.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The island was fairly ugly when I began, but when I'm done with it, it will be beautiful.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "When I'm at school or work, I lay out a mental agenda for what I will do next time I play.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "If I traded with others online, it would be more about meeting people than actually trading.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Honestly, this game is not very important to me.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It is crucial that I complete my museum at some point in the future.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Animal Crossing is a sandbox game focused around creation and design.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I don't plan my in-game week. Every day is a surprise.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "If Animal Crossing were a single player game, it would be much less fun.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "My design choices are deliberate and methodical.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "I don't want to change my island too much. It's nice how it is.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Completing the catalog is a realistic goal for me, and I can do it.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I would host a tournament to play minigames with other players.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Leaving the game for several days is no worry.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I would utilize a duplication exploit if I had the chance.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I use online communities like reddit to engage with other players.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I always indulge in the seasonal events, even if they aren't productive.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "My ideal island is precise and functional.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "I choose my villagers based upon the designs of their homes.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 5
+        }
+    },
+    {
+        "question": "I often stop what I'm doing to enjoy the hourly music.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I spend a lot of time playing this game for therapeutic or relaxation purposes.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It seems fun to invite strangers to my island for no particular reason.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "I often begin an activity, get bored with it, then move onto another activity.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "I use many custom designs, and most of them are made by me.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "If I need a specific piece of furniture for my house, I will get my hands on it. No substitutes.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Achieving my personal goals is more important than sharing them.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "There is no rhyme or reason to my terraforming. I just build things when and where I feel like it.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "It is always more fun to be playing with a friend.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "When designing, I care more about aesthetics than symbolic meaning.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I spend a lot of time building things and then tearing them down, over and over again.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -10
+        }
+    },
+    {
+        "question": "I don't care what others think of my island. Only my opinion matters.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Usually, I would rather chat with my villagers than develop my island.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It is fun to lazily walk around the museum reading the creature descriptions.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": -5
+        }
+    },
+    {
+        "question": "My achievements in game are part of my real-world identity.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Going to Harv's Island to set up scenes and take photos is lots of fun.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Everything on my island should fit together neatly and cleanly.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "If multiplayer was faster and worked better in general, my life would be so much easier.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I have a set theme for my island and I don't deviate from it.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "I limit my playtime so I don't become obsessed.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "When visiting a Nook mystery island, I quickly grab what I need and get out.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I would give away valuable items to strangers, just for the heck of it.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "My house storage is overflowing and disorganized.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "I bond with my villagers on a personal level, as if they're not just lines of code.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I have a deep interest in the inner workings of the game, including datamined mechanics.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I love to show off my creations to others.",
+        "effect": {
+            "econ": 5,
+            "dipl": 10,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Nothing beats a symmetrical, orderly system of walking paths.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": 10
+        }
+    },
+    {
+        "question": "I see the online community as competitive, and I indulge this. A race for accomplishment.",
+        "effect": {
+            "econ": 10,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It is unfair that I have to interact with others to get island-specific Nook Shopping items.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I actively game the stalk market to get loads of bells.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "I manage to accomplish a lot without the need for plans or schedules.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Designing great things and sharing them with other players is what Animal Crossing is all about.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Money-making techniques stress me out. I'd rather go at a steady pace.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "When the seasons change, I care more about enjoying the scenery than obtaining seasonal items.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I build only for the beautiful, finished product. The process itself is loathsome.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I prefer to keep to myself and focus on personal progress.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I don't care about achievement. I just want to relax.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Measure twice, cut once. Important tasks should be thought about beforehand.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "I change my clothing frequently and wildly to show other players my ridiculous side.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": -5,
+            "scty": -5
+        }
+    },
+    {
+        "question": "I do what I want, when I want, how I want. Order is no fun.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "I care about collecting all of the reactions so I can better express my feelings to others.",
+        "effect": {
+            "econ": 5,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    }
+];
